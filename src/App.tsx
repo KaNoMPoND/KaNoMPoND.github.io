b1249612894dbc39a7dbd87a0abf9c269e683912
KUNANON HIRUNRATTANAPORN <kunanon.hiru@bu.ac.th>
2024-09-12 01:17:46 +0700

    create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-        
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex px-7 bg-red-400 w-screen h-screen">
+        <div className="w-1/2 text-center bg-black text-white ">
+          leftcontent
+          <div className="border w-4/4 h-2/6 ">photo</div>
+          <div  className="border  ">story of life</div>
+          <div className="border  ">education</div>
+          <div className="border  ">Award</div>
+        </div>
+        <div className="w-1/2 text-center">
+          rightcontent
+          <div className="border">Name</div>
+          <div className="border">contact profile</div>
+          <div className="border">work experience</div>
+          <div className="border">expertise</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
