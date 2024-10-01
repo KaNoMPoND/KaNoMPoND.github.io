548282a2f430cedf481d1e7d2f0bbf9dc2a1f769
KUNANON HIRUNRATTANAPORN <kunanon.hiru@bu.ac.th>
2024-10-01 16:29:25 +0700

    add information & Sort layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,63 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="grid bg-gradient-to-b from-blue-950 via-blue-900 to-blue-600 font-Oswald">
+      <div className="grid bg-gradient-to-b from-blue-950 via-blue-900 to-blue-600">
         <div>photo</div>
         <div className="text-white">
           <div className=" text-4xl text-center pt-5">
             Kunanon Hirunrattanaporn
           </div>
 
-          <div className="text-2xl px-8">
+          <div className="text-2xl mx-8 mb-10 mt-7">
             About Me
             <hr></hr>
-            <div className="text-xl">Location</div>
-            <div className="text-xl">Language</div>
+            <div className="font-Oswald text-wrap text-xl pl-9 pb-2">
+              <div>Location : (icon)</div>
+              <div className="text-xs pl-3">
+                ซอย หมู่บ้านภูมิใจนิเวศ 3 ซอย 1/1 ต.ในคลองบางปลากด
+                อ.พระสมุทรเจดีย์ จ.สมุทรปราการ 10290
+              </div>
+              <div>Language(icon)</div>
+              <div className="text-xs pl-3">English & Thai Language</div>
+            </div>
           </div>
 
-          <div className="text-2xl px-8 ">
+          <div className="text-2xl mx-8 mb-10 ">
             Hobby
             <hr></hr>
-            <div className="text-xl">Watch Movie</div>
-            <div className="text-xl">Play Game</div>
-            <div className="text-xl">Home Workout</div>
+            <div className="font-Oswald text-xl pl-9">
+              <div className="pb-2">(icon)Watch Movie</div>
+              <div className="pb-2">(icon)Play Game</div>
+              <div className="pb-2">(icon)Home Workout</div>
+            </div>
           </div>
 
-          <div className="text-2xl px-8">
+          <div className="text-2xl mx-8 mb-10">
             Contact Profile
             <hr></hr>
-            <div className="text-xl">Email :</div>
-            <div className="text-xl">Tel.</div>
-            <div className="text-xl">FB :</div>
+            <div className="font-Oswald text-xl pl-9">
+              <div className="pb-2">Email(icon) : kunanon.hn@gmail.com</div>
+              <div className="pb-2">Tel.(icon) : 0955487274</div>
+              <div className="pb-2">FB(icon) : Kunanoni Hirunrattanaporn</div>
+            </div>
           </div>
 
-          <div className="text-2xl px-8">
+          <div className="text-2xl mx-8 mb-10">
             Education
             <hr></hr>
-            <div className="text-xl">
-              Junior High School 2015-2017 : Saint Marry school
-            </div>
-            <div className="text-xl text-nowrap">
-              High School 2018-2020 : Wisutthi Kasattree School
-            </div>
-            <div className="text-xl">
-              University 2022-2026 : Bangkok University
+            <div className="font-Oswald text-wrap text-xl pl-3">
+              <div>Junior High School 2015-2017 :</div>
+              <div className="pb-2 pl-4">Saint Marry school</div>
+              <div>High School 2018-2020 :</div>
+              <div className="pb-2 pl-4">Wisutthi Kasattree School</div>
+              <div>University 2022-2026 :</div>
+              <div className="pb-2 pl-4">Bangkok University</div>
             </div>
           </div>
-          <div className="text-2xl px-8">Skill</div>
-          <hr></hr>
+          <div className="text-2xl mx-8 mb-10">
+            Skill
+            <hr></hr>
+          </div>
         </div>
       </div>
     </>
