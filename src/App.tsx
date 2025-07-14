01872fee5157ff93b8472f137868ce3806eec35a
KUNANON HIRUNRATTANAPORN <kunanon.hiru@bu.ac.th>
2025-07-14 20:19:50 +0700

    Chang tell & email

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
   const activitiesRef = useRef<HTMLDivElement>(null);
   const certificateRef = useRef<HTMLDivElement>(null);
 
+
   // Typewriter Effect
   const typewriterText = "Student Of Bangkok University\nCyber security Major.";
   const [displayedText, setDisplayedText] = React.useState("");
@@ -255,7 +256,7 @@ function App() {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                   </svg>
                 </span>
-                <a href="tel:+66099999999" className="text-white hover:text-pink-400 transition">+66(0)9.9999.9999</a>
+                <a href="tel:0955487274" className="text-white hover:text-pink-400 transition">095-548-7274</a>
               </div>
               
               <div className="flex items-center gap-3">
@@ -264,7 +265,7 @@ function App() {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                   </svg>
                 </span>
-                <a href="mailto:hello@yourdomain.com" className="text-white hover:text-pink-400 transition">hello@yourdomain.com</a>
+                <a href="mailto:kunanon.hn@gmail.com" className="text-white hover:text-pink-400 transition">kunanon.hn@gmail.com</a>
               </div>
             </div>
           </div>
